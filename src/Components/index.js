4d71e5ceb3eef8fc3d5173a05a264392d915331b
kenil-kanani <kenilkananigecbvn@gmail.com>
2023-09-28 22:39:25 +0530

    Added CardDetail Components

---
+++ src/Components/index.js
@@ -0,0 +1,9 @@
+import Scholarship from './Scholarship/Scholarship'
+import Navbar from './Nabvar/Navbar'
+import CardDetails from './CardDetails/cardDetails'
+
+export {
+    Scholarship,
+    Navbar,
+    CardDetails
+}
